3fa32d3f8e6a738aba9dcfa63152560432025498
jiten <jitendrak.samal@gmail.com>
2020-11-12 10:29:30 -0700

    All commit

---
+++ src/com/projectcarv/StringTest/OverLoadingTest.java
@@ -0,0 +1,20 @@
+package com.projectcarv.StringTest;
+
+public class OverLoadingTest {
+
+	public void getString(String a) {
+		System.out.println("getString");
+	}
+	
+	public void getString(StringBuffer a) {
+		System.out.println("getStringBuffer");
+	}
+	
+	
+	public static void main(String[] args) {
+		OverLoadingTest olt = new OverLoadingTest();
+		olt.getString("abc");
+
+	}
+
+}
