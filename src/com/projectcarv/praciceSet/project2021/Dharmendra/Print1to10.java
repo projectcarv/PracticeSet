80a4b96df5aa5101b9ebb60c08d212d8ebd8a813
jiten <jitendrak.samal@gmail.com>
2020-11-18 10:08:20 -0700

    By Jitendra for R&D

---
+++ src/com/projectcarv/praciceSet/project2021/Dharmendra/Print1to10.java
@@ -0,0 +1,12 @@
+package com.projectcarv.praciceSet.project2021.Dharmendra;
+
+public class Print1to10 {
+	
+	public static void main(String args[]){
+		System.out.println("Counting from 0 to 9 as - ");
+		for(int i=0;i<10;i++){
+			System.out.println(i);
+		}
+	}
+
+}
