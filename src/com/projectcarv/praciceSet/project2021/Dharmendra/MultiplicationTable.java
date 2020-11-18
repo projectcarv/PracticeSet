d5498ebed7cdd602093eeae8c579396628676a42
jstudentGit <jitendrakumarsamal1@gmail.com>
2020-11-18 11:09:18 -0700

    multiplication table added to by Jitendra

---
+++ src/com/projectcarv/praciceSet/project2021/Dharmendra/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.projectcarv.praciceSet.project2021.Dharmendra;
+
+public class MultiplicationTable{
+	public static void main(String args[]){
+		System.out.println("Multiplication table for 2");
+		int j=2;
+		for(int i=1;i<11;i++){
+			System.out.println(j +"*"+ i +"="+ j*i);
+		}
+	}
+
+}
