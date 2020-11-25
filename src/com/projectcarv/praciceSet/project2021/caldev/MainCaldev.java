c59530f3b36b6721a0c09b4c8e629b4f49fc5a71
jitendra-git20 <jitendrak.samal@gmail.com>
2020-11-25 14:15:51 -0700

    commit for caldev project

---
+++ src/com/projectcarv/praciceSet/project2021/caldev/MainCaldev.java
@@ -0,0 +1,28 @@
+package com.projectcarv.praciceSet.project2021.caldev;
+
+import java.util.Scanner;
+
+public class MainCaldev {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Please enter a number - ");
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		System.out.println("Please enter another number - ");
+		int j = sc.nextInt();
+		
+		// Call to add method implemented by Abhishikta/twinkle
+		String addResult = add(i,j);
+		System.out.println(addResult );
+		
+		
+		// Call to substract method implemented by Manisha
+		String subResult = substract(i,j);
+		System.out.println(subResult );
+		
+	}
+
+}
+
