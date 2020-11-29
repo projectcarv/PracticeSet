dad11b92561e4a937dcd34afec1608e612335e01
amarnathsocial <amarnathsocial@gmail.com>
2020-11-29 19:04:47 +0530

    ValidateModule develop using map.. by Amarnath

--- src/com/projectcarv/praciceSet/project2021/caldev/Validation.java
+++ src/com/projectcarv/praciceSet/project2021/caldev/Validation.java
@@ -2,6 +2,8 @@ package com.projectcarv.praciceSet.project2021.caldev;
 
 public class Validation {
 	public static final String errorCons ="Both are positive integer";
+	public static final String errormsgforI ="Please enter positive integer in place of : ";
+	public static final String errormsgforJ ="Please enter positive integer in place of : ";
 
 	public String validate(int i, int j) { // -4, -5
 		String result = "";
@@ -13,11 +15,11 @@ public class Validation {
 
 		}else if (i < 0) {
 			
-			result = "Please enter positive integer inplace of " +i;
+			result = errormsgforI +i;
 
 		}else if (j < 0 ) {
 
-			result = "Please enter positive integer inplace of " +j;
+			result = errormsgforJ +j;
 		}
 		return result;
 	}
---
+++ src/com/projectcarv/praciceSet/project2021/caldev/mapApproach/Validation.java
@@ -0,0 +1,21 @@
+package com.projectcarv.praciceSet.project2021.caldev.mapApproach;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Validation {
+	public static final String errormsgforBoth ="Both are Positive : ";
+	public static final String errormsgforI ="Please enter positive integer in place of : ";
+	public static final String errormsgforJ ="Please enter positive integer in place of : ";
+
+	public String validate(int i, int j) { // -4, -5
+		Map<boolean, String> map= new HashMap<>();
+		if(i>0 && j>0){
+			map.put("true","errormsgforBoth");
+		}else if(i<0){
+			map.put("false","errormsgforI" +i);
+		}else if(j<0){
+			map.put("false","errormsgforJ" +j);
+		}
+		return (map);
+	}
+}
