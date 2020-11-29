2969df414a1c7246ee5532c68c4eb676ca23d1cc
jitendra-git20 <jitendrak.samal@gmail.com>
2020-11-29 12:40:06 -0700

    Compile time error rectified for Manisha Files

--- src/com/projectcarv/praciceSet/project2021/manishalenaka1/Helloworld.java
+++ src/com/projectcarv/praciceSet/project2021/manishalenaka1/Helloworld.java
@@ -1,4 +1,5 @@
-Class HelloWorld{
+package com.projectcarv.praciceSet.project2021.manishalenaka1;
+class HelloWorld{
 	public static void main(String args[]){
 	System.out.println("Hello I Am Manisha...!!!");
 	}
