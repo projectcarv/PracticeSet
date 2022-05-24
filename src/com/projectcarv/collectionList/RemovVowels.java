e1657c08b765f7d7eb9c1410733eaf56963b31f2
jstudentGit <jstudentGit>
2022-05-24 09:44:40 -0700

    New file added

---
+++ src/com/projectcarv/collectionList/RemovVowels.java
@@ -0,0 +1,28 @@
+package com.manan.dataStructures;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RemovVowels {
+
+	static String remVowel(String str)
+    {
+         Character vowels[] = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'};
+         List<Character> al = Arrays.asList(vowels); 
+         StringBuffer sb = new StringBuffer(str);
+         for (int i = 0; i < sb.length(); i++) {   
+             if(al.contains(sb.charAt(i))){
+                sb.replace(i, i+1, "") ;
+                i--;
+             }
+        }   
+        return sb.toString();
+    }
+    public static void main(String[] args)
+    {
+        String str = "Manan - tech";
+         
+        System.out.println(remVowel(str));
+    }
+
+}
